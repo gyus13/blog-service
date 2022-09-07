f5d660aa2d2ffad56f32391fc8b8119f456df896
gyus13 <gyusss13@naver.com>
2022-09-07 12:31:10 +0900

    Feat: Board 수정 api

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { PostSignUpRequesterRequest } from '../user/dto/post-sign-up-user.request.dto';
 import { PostBoardRequest } from './dto/post-board.request.dto';
+import { BoardService } from './board.service';
 
 @Controller('board')
 export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
   /**
    * description : 게시글 생성 API
    * @param PostBoardRequestDto
@@ -30,5 +32,35 @@ export class BoardController {
   @Post()
   postSignUp(@Body() postBoardRequest: PostBoardRequest) {
     return this.boardService.createBoard(postBoardRequest);
+  }
+
+  /**
+   * description : 게시글 수정 API
+   * @param PostBoardRequestDto
+   * @returns non-exist
+   */
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+  })
+  @ApiResponse({
+    status: 401,
+    description: '인증되지 않은 권한 입니다.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: '서버 에러',
+  })
+  @ApiOperation({ summary: '게시글 수정 API' })
+  @ApiBody({
+    description: '게시글 수정 DTO',
+    type: PostBoardRequest,
+  })
+  @Patch(':/id')
+  PatchBoard(
+    @Body() postBoardRequest: PostBoardRequest,
+    @Param('id') id: number,
+  ) {
+    return this.boardService.editBoard(postBoardRequest, id);
   }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -21,12 +21,12 @@ export class BoardService {
     private readonly userEntityRepository: Repository<UserEntity>,
   ) {}
 
-  async createBoard(postBoardRequest: PostBoardRequest) {
+  async editBoard(postBoardRequest: PostBoardRequest, id) {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
-      // 입력한 이메일에 해당하는 유저값 추출
+      // 입력한 패스워드에 해당하는 유저값 추출
       const user = await this.userEntityRepository.findOne({
         where: { password: postBoardRequest.password },
       });
@@ -50,7 +50,70 @@ export class BoardService {
         return response.UNAUTHORIZED;
       }
 
-      // GridgeRequester 인스턴스 생성후, 정보 담는 부분
+      // board 수정
+      await queryRunner.manager.update(
+        BoardEntity,
+        { id: id },
+        { title: postBoardRequest.title },
+      );
+
+      await queryRunner.manager.update(
+        BoardEntity,
+        { id: id },
+        { text: postBoardRequest.text },
+      );
+
+      // Response의 result 객체에 Data를 담는 부분
+      const data = {
+        message: '게시글이 수정 되었습니다.',
+      };
+
+      const result = makeResponse(response.SUCCESS, data);
+
+      // Commit
+      await queryRunner.commitTransaction();
+
+      return result;
+    } catch (error) {
+      // Rollback
+      await queryRunner.rollbackTransaction();
+      console.log(error);
+      return response.ERROR;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async createBoard(postBoardRequest: PostBoardRequest) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      // 입력한 패스워드에 해당하는 유저값 추출
+      const user = await this.userEntityRepository.findOne({
+        where: { password: postBoardRequest.password },
+      });
+
+      // 제목 길이 검증
+      if (postBoardRequest.title.length > 20) {
+        return response.UNAUTHORIZED;
+      }
+      // 본문 길이 검증
+      if (postBoardRequest.text.length > 200) {
+        return response.UNAUTHORIZED;
+      }
+      // 비밀번호 검증
+      const reg = /^(?=.*?[0-9]).{6,}$/;
+      if (!reg.test(postBoardRequest.password)) {
+        return response.UNAUTHORIZED;
+      }
+      const hashedPassword = await bcrypt.hash(postBoardRequest.password, 3);
+
+      if (user.password != hashedPassword) {
+        return response.UNAUTHORIZED;
+      }
+
+      // board 인스턴스 생성후, 정보 담는 부분
       const board = new BoardEntity();
       board.title = postBoardRequest.title;
       board.text = postBoardRequest.text;
